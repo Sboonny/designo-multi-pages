5d3dfe922d24497be04cac040d370ad861ecce2b
Sboonny <88248797+Sboonny@users.noreply.github.com>
2022-05-06 18:28:17 +0200

    Started the challenge
    added assets made files and installed vue and typescript

---
+++ src/Home.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import HeaderVue from './components/Header.vue';
+import FooterVue from './components/Footer.vue';
+</script>
+
+
+<template>
+ <HeaderVue />
+ <FooterVue />
+</template>
---
+++ src/components/Header.vue
@@ -0,0 +1,35 @@
+<template>
+<a class="skip-to-content" href="#main">Skip to content</a>
+<header>
+  <div>
+<img src="../assets/logo-dark.png" alt="logo">
+</div>
+<nav>
+   <button class="mobile-nav-toggle" aria-controls="primary-navigation"><span class="sr-only" aria-expanded="false" role="button">Menu</span></button>
+   <ul
+          id="primary-navigation"
+          data-visible="false"
+          class="primary-navigation underline-indicators flex"
+        >
+          <li class="active">
+            <a
+              class="ff-sans-cond uppercase text-white letter-spacing-2"
+              >Company</a
+            >
+          </li>
+          <li>
+            <a
+              class="ff-sans-cond uppercase text-white letter-spacing-2"
+              >Locations</a
+            >
+          </li>
+          <li>
+            <a
+              class="ff-sans-cond uppercase text-white letter-spacing-2"
+              >Contact</a
+            >
+          </li>
+        </ul>
+</nav>
+</header>
+</template>
