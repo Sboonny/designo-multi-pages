1911a9a647ad03df1eaf5817aa8526137bd108d9
Sboonny <88248797+Sboonny@users.noreply.github.com>
2022-05-10 17:49:48 +0200

    finish grid responsive layout for the footer
    removed scope and javascript to make the page carry less load

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,21 +1,3 @@
-<script lang="ts">
-import { defineComponent } from "vue";
-
-export default defineComponent({
-  data: () => {
-    return {
-      icons: [
-        {img: "../assets/Icons/icon-instagram.svg", alt: "instagram"},
-        {img: "../assets/Icons/icon-facebook.svg", alt: "facebook"},
-        {img: "../assets/Icons/icon-twitter.svg", alt: "twitter"},
-        {img: "../assets/Icons/icon-youtube.svg", alt: "youtube"},
-        {img: "../assets/Icons/icon-pinterest.svg", alt: "pinterest"}
-        ] as any
-    };
-  },
-});
-</script>
-
 <template>
   <footer class="grid footer-layout">
     <div class="logo-area">
@@ -25,18 +7,18 @@ export default defineComponent({
     <div class="company-nav-area">
       <ul class="underline-indicators flex primary-navigation" role="list">
         <li>
-          <a class="ff-sans-cond uppercase letter-spacing-2">Company</a>
+          <a class="ff-sans-cond uppercase">Company</a>
         </li>
         <li>
-          <a class="ff-sans-cond uppercase letter-spacing-2">Locations</a>
+          <a class="ff-sans-cond uppercase">Locations</a>
         </li>
         <li>
-          <a class="ff-sans-cond uppercase letter-spacing-2">Contact</a>
+          <a class="ff-sans-cond uppercase">Contact</a>
         </li>
       </ul>
     </div>
 
-    <div class="addres-area">
+    <div class="address-area">
       <ol role="list">
         <li>Designo Central Office</li>
         <li>3886 Wellington Street</li>
@@ -51,19 +33,45 @@ export default defineComponent({
       </ol>
     </div>
 
-
     <div class="flex icons-area">
-      <img v-for="(icon, i) in icons" :key="i" :src="icon.img" :alt="icon.alt">
+      <img src="../assets/Icons/icon-instagram.svg" alt="instagram" />
+      <img src="../assets/Icons/icon-facebook.svg" alt="facebook" />
+      <img src="../assets/Icons/icon-twitter.svg" alt="twitter" />
+      <img src="../assets/Icons/icon-youtube.svg" alt="youtube" />
+      <img src="../assets/Icons/icon-pinterest.svg" alt="pinterest" />
     </div>
   </footer>
 </template>
 
 
-<style scoped>
+<style>
 .footer-layout {
   background-color: hsl(var(--dark-blue));
   color: white;
+  place-items: center;
 }
-
+@media (min-width: 35em) {
+  .logo-area {
+   grid-area: logo;
+  }
+  .company-nav-area {
+   grid-area: nav;
+  }
+  .address-area {
+    grid-area: address;
+  }
+  .content-area {
+    grid-area: contentarea;
+  }
+  .icons-area {
+    grid-area: icons;
+  }
+  
+  .footer-layout {
+    grid-template-columns: repeat(4, 1fr);
+    grid-template-areas: "logo . nav nav"
+                          "address contentarea . icons";
+  }
+}
 
 </style>
