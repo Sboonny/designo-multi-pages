3bc5c2de8579544a7abf342bf9d016b27a4c139d
Sboonny <MuhammedElruby@gmail.com>
2022-05-18 20:46:26 +0200

    before the bug

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,2 +1,32 @@
 import Vue from 'vue'
 import { createRouter, createWebHistory} from 'vue-router'
+import Home from '../views/Home.vue'
+import About from '../views/About.vue'
+import Contact from '../views/Contact.vue'
+import { def } from '@vue/shared'
+
+
+ const routes = [
+     {
+         path: '/',
+         name: 'Home',
+         componenent: Home
+     },
+     {
+         path: "/about",
+         name: "about",
+         componenent: About
+     },
+     {
+        path: "/contact",
+        name: "contact",
+        componenent: Contact
+    }
+ ]
+
+ const router = VueRouter.createRouter({
+    history: VueRouter.createWebHashHistory(),
+    routes 
+ })
+
+ export default router
