0bd23f300ff94c7035b51498181945581c8bf2d0
Sboonny <MuhammedElruby@gmail.com>
2022-05-20 19:30:41 +0200

    before the storm

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,15 +2,9 @@ import VueRouter from 'vue-router'
 import Home from '../views/HomeContent.vue'
 import About from '../views/About.vue'
 import Contact from '../views/Contact.vue'
-import { Component } from 'vue'
 
-interface routeType {
-path: string,
-name: string,
-componenent: Component
-}
 
-const routes: routeType = [
+const routes = [
     {
         path: '/',
         name: 'Home',
